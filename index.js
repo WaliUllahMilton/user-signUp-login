d6713eff1d6a1ebe3573e6dd90c7caa0dccd1d27
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-07-02 19:26:07 +0600

    login add

--- index.js
+++ index.js
@@ -49,3 +49,29 @@ app.post("/registration",async(req,res)=>{
         console.log(error)
     }
 })
+
+app.post("/login",async (req,res)=>{
+    try {
+        const data = await users.findOne({"email":req.body.email})
+        if(data){
+            if(data.number=== req.body.number){
+                return res.status(200).json({
+                    "massage": "succes",
+                    "data": data
+                })   
+            }
+            return res.status(401).json({
+                "massage" : "password wrong",
+                "data" : data
+            })
+        }else{
+            return res.status(404).json({
+                "massage" : "user not exist",
+                "data" : data
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
