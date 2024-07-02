74150f5c7f4aae7e8dacff361d10f39346a2abf1
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-07-02 19:14:58 +0600

    registration add

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 
@@ -7,11 +6,46 @@ const DB = mongoose.connect("mongodb+srv://milton:12345@cluster0.ukmnle3.mongodb
 DB.then(console.log("Mongoose connected bro"));
 
 const app = express()
+// Middleware to parse JSON and URL-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-
-app.listen(3001,()=>{
+app.listen(8000,()=>{
     console.log("server also running bro")
 })
 
+const userSchema = mongoose.Schema({
+    name:String,
+    email:{
+        type : String,
+    },
+    number:Number
+})
 
+const users = mongoose.model("users",userSchema)
+
+// route 
+app.get("/",(req,res)=>{
+    res.send("running")
+})
+
+app.post("/registration",async(req,res)=>{
+    try {
+        const checkExisting = await users.findOne({"email":req.body.email});
+        if(checkExisting){
+            // return res.send(req.body.email) obj pathate hole .json use krte hobe
+            return res.status(400).json({error:"user ase",email :req.body.email})
+        }else{
+            const data = new users({
+                name : req.body.name,
+                email : req.body.email,
+                number : req.body.number,
+            })
+            await data.save();
+            return res.send(data)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
